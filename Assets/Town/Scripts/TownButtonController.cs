51202d202c2f88f591224e64f7e405d7de5f116c
gregolopogus <gregoryperry627@gmail.com>
2017-01-02 21:54:22 -0800

    Sprites and Tavern
    Fixed buggy sprites, added shadow to gnome chompy, added sprite for
    journal, made it so you can sleep at the tavern to get health

--- Assets/Town/Scripts/TownButtonController.cs
+++ Assets/Town/Scripts/TownButtonController.cs
@@ -6,6 +6,7 @@ public class TownButtonController : MonoBehaviour {
     TownWatchdog town;
     BountyBoard bountyBoard;
     Shop shop;
+    Tavern tavern;
     public Collider2D bountyBoardCollider, tavernCollider, shopCollider, forestCollider;
 
 	// Use this for initialization
@@ -13,6 +14,7 @@ public class TownButtonController : MonoBehaviour {
         town = GetComponent<TownWatchdog>();
         bountyBoard = GetComponent<BountyBoard>();
         shop = GetComponent<Shop>();
+        tavern = GetComponent<Tavern>();
 	}
 	
 	// Update is called once per frame
@@ -49,6 +51,7 @@ public class TownButtonController : MonoBehaviour {
         } else if (collider == bountyBoardCollider) {
             bountyBoard.openBounties();
         } else if (collider == tavernCollider) {
+            tavern.openTavernPopUp();
             
         } else if (collider == forestCollider) {
             town.showForestPopUp();
