f013f91536f1711509dc0fc53bbbe9c9bc2bf3cb
gregolopogus <gregoryperry627@gmail.com>
2017-03-30 10:52:04 -0700

    Added camp icon

--- Assets/Persistent/Scripts/StoryOverlord.cs
+++ Assets/Persistent/Scripts/StoryOverlord.cs
@@ -11,7 +11,7 @@ public class StoryOverlord : MonoBehaviour {
     public static item reward;
     public static string questStartDialogue;
     public static string questEndDialogue;
-    public static int characterSprite;
+    public static Sprite characterSprite;
 	// Use this for initialization
 	void Start () {
 	
@@ -32,9 +32,15 @@ public class StoryOverlord : MonoBehaviour {
             // Enemy Spawns: GnomeChompy
             // Boss: GreenChompy
             // Reward: Bronze Sword
-            characterSprite = 0; // Old Guy
-            questStartDialogue = "";
-            questEndDialogue = "";
+            characterSprite = Resources.Load("Characters/Moik") as Sprite; // Old Guy
+            questStartDialogue = "Well, if you're set on going on an adventure you'll need a sword. Tell you what, I'll give you my old sword if you can "+
+                "get it back from those pesky gnomes that stole it. They should be out in the garden somewhere, and watch out they like to bite.";
+
+            questEndDialogue = "Well look at that, you actually did it. Hope that old thing works for you, I'd give you my new sword but its actually nice and "+
+                "I like it. Anyway, if you're set on going on this adventure the mountain is 100km away so I hope you brought your walking shoes cause you're in " +
+                "for quite the trip. The next town is a few kilometers down the road, you can't miss it. I would start there is I were you. Well, good luck, and "+
+                "don't come crying to me when you get yourself killed.";
+               
             firstEnemy = 0;
             lastEnemy = 0;
             reward = ItemList.itemMasterList[ItemList.BRONZE_SWORD];
@@ -51,6 +57,8 @@ public class StoryOverlord : MonoBehaviour {
             // Enemy Spawns: GnomeChompy, GreenChompy, Goblin, Red Goblin
             // Boss: Goblin Champ
             // Reward: Iron Sword
+            questStartDialogue = "Hey new quest here";
+            questEndDialogue = "Woo u did it";
             firstEnemy = 0;
             lastEnemy = 3;
             int goldReward = 0;
