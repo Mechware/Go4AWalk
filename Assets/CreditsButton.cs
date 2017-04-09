29c0e5e40578e1d03da120818128ee8cc597f8a7
gregolopogus <gregoryperry627@gmail.com>
2017-04-09 15:43:32 -0700

    Credits
    Added credits to the journal menu

---
+++ Assets/CreditsButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+    public GameObject creditsPanel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void showCredits() {
+        creditsPanel.SetActive(true);
+    }
+    public void hideCredits() {
+        creditsPanel.SetActive(false);
+    }
+}
