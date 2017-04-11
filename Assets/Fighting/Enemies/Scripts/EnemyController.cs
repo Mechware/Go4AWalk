854ad4c5c58d0cf9ec3639dbe24fcde41cd04467
Michael Demone <m_d_emone@hotmail.com>
2017-04-11 15:49:39 -0700

    Created a controller script so each enemy didn't need their own controller
    Also added this script to each enemy and renamed their animations so
    they fit the mold.

---
+++ Assets/Fighting/Enemies/Scripts/EnemyController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyController : MonoBehaviour {
+
+    private AnimatorOverrideController controller;
+    public AnimationClip[] replacementClips;
+
+	// Use this for initialization
+	void Start () {
+        Animator animator = GetComponentInChildren<Animator>();
+        controller = new AnimatorOverrideController();
+        controller.runtimeAnimatorController = animator.runtimeAnimatorController;
+        animator.runtimeAnimatorController = controller;
+        setController();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void setController() {
+
+        // get the number of clips on the AnimatorOverrideController
+        int numClips = controller.clips.Length;
+
+        // make a new array of that length
+        AnimationClipPair[] newClips = new AnimationClipPair[numClips];
+
+        // loop through each clip pair
+        for (int i = 0 ; i < numClips ; i++) {
+            AnimationClipPair pair = controller.clips[i];
+
+            // get the appropriate clip from your model
+            string name = pair.originalClip.name;
+            AnimationClip clip = getClipWithName(name);
+            if(clip == null) {
+                print("ERR: Couldn't find clip with name: " + name);
+                continue;
+            }
+
+            // if there is a model clip, 
+            // set it as the overrideClip on the pair
+            if (clip != null) {
+                pair.overrideClip = clip;
+            }
+
+            // save the pair to your new clips array
+            newClips[i] = pair;
+
+        }
+
+        // when everything is properly set, 
+        // replace the entire clips array          
+        controller.clips = newClips;
+    }
+
+    AnimationClip getClipWithName(string name) {
+        foreach(AnimationClip clip in replacementClips) {
+            if (name.Equals(clip.name))
+                return clip;
+        }
+        return null;
+    }
+}
