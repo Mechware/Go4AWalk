17ff92f1190fa4f87b535b233e3ab0efbf679bef
Michael Demone <m_d_emone@hotmail.com>
2017-04-14 16:12:12 -0700

    Player can no longer rest while enemies are following

--- Assets/Walking/Scripts/GoToCampFromWalking.cs
+++ Assets/Walking/Scripts/GoToCampFromWalking.cs
@@ -9,6 +9,10 @@ public class GoToCampFromWalking : MonoBehaviour {
     // Method for going into town and ending the quest
     public void goToCamp() {
     
+        if(EnemyWatchdog.enemiesQueue.Count != 0) {
+            PopUp.instance.showPopUp("You cannot sleep while monsters are following you!", new string[] { "Okay" });
+            return;
+        }
         PopUp.instance.showPopUp("WOULD YOU LIKE TO MAKE CAMP FOR THE NIGHT?",
             new string[] { "YES", "NO" },
             new Action[] { makeCamp, () => { } });
