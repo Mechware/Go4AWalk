3564e007bb23a95e08f1073502900d0353a6f045
Michael Demone <m_d_emone@hotmail.com>
2017-04-19 15:36:58 -0700

    Added intro/ spiffy chicken games scene
    WOOOOOOOOOOOOOOOO!

---
+++ Assets/Intro/IntroToMainGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroToMainGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(switchScenes());
+	}
+	
+	IEnumerator switchScenes() {
+        yield return new WaitForSeconds(2);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("WalkingScreen");
+    }
+}
